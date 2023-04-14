83b5fb500d1471df7861204bae355b7ab0af45b2
Shreyas singh <71962705+shreyassingh96@users.noreply.github.com>
2023-04-14 11:45:45 -0400

    Add project files.

---
+++ Quiz4/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quiz 4");
+Console.WriteLine("Name: Shreyas Hirakumar Singh");
+Console.WriteLine("Student no: 301239523");
