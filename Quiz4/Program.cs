7e660522e270cf3e525b0d9e3d127ddb98e4f6aa
Shreyas singh <71962705+shreyassingh96@users.noreply.github.com>
2023-04-14 11:51:16 -0400

    Second commit with added database

--- Quiz4/Program.cs
+++ Quiz4/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Quiz 4");
 Console.WriteLine("Name: Shreyas Hirakumar Singh");
 Console.WriteLine("Student no: 301239523");
+Console.WriteLine("Database access has already been done on April 14, 2023 by Shreyas Hirakumar Singh");
+Console.ReadLine();
